3b02367db625722127a94f92544182b50646b9b6
lucassr-dev <lucassr.job@gmail.com>
2024-08-30 21:29:17 -0300

    Implementação das Rotas de Autenticação

--- src/server.js
+++ src/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const sequelize = require('./config/database');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
