2359332ec178c9ae5e24837df80f04c2de172ae1
lucassr-dev <lucassr.job@gmail.com>
2024-08-30 23:27:57 -0300

    Template inicial FrontEnd com Vue.js 3

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <h1 class="text-2xl font-bold text-center my-4">Gerenciador de Despesas</h1>
+  </header>
+
+  <RouterView />
+</template>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <h2 class="text-xl text-center my-4">Bem-vindo ao Gerenciador de Despesas</h2>
+    <p class="text-center">Aqui você poderá gerenciar suas despesas compartilhadas de forma fácil e eficiente.</p>
+  </main>
+</template>
